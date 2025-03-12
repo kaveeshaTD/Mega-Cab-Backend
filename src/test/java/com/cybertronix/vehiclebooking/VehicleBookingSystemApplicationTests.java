e6054ae52cf0f811ce8e9db9b0eae067b7d08a6d
kaveesha.thathsara <kaveesha.thathsara@itelasoft.com>
2025-03-12 10:56:57 +0530

    initial commite

---
+++ src/test/java/com/cybertronix/vehiclebooking/VehicleBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertronix.vehiclebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
